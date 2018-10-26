52524fac0c3aa4aa3b0a169ceab4482dcacf2129
Nicolas Modrzyk <hellonico@gmail.com>
2018-10-26 14:42:11 +0900

    support for linux

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :main zebra.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts [~(str "-Djava.library.path=lib/"  (apply str (take 3 (get (System/getProperties) "os.name"))))]
+  :jvm-opts [~(str "-Djava.library.path=lib/"  (.toLowerCase (apply str (take 3 (get (System/getProperties) "os.name")))))]
   :repositories [["vendredi" {:url "https://repository.hellonico.info/repository/hellonico/" :creds :gpg}]]
   :dependencies [[com.google/ortools "6.7.4981"]
                  [com.google.protobuf/protobuf-java "3.6.1"]
