1f59f6a0db25a09c02e469eb22bb35a689db46e2
Nicolas Modrzyk <nico@karabiner-software.com>
2018-10-26 15:33:29 +0900

    clean up

--- src/zebra/core.clj
+++ src/zebra/core.clj
@@ -1,9 +1,7 @@
 (ns zebra.core
-  ; (:import [clojure.lang RT])
   (:import [com.google.ortools.linearsolver MPSolver$OptimizationProblemType MPSolver]))
 
-; (org.scijava.nativelib.NativeLoader/loadLibrary "dimacs" nil)
-; (org.scijava.nativelib.NativeLoader/loadLibrary "fap" nil)
+(def loaded (atom false))
 (defn load-natives []
   (let [os (.toLowerCase (apply str (take 3 (get (System/getProperties) "os.name"))))]
     (if (or (= "lin" os) (= "mac" os))
@@ -11,22 +9,11 @@
         (org.scijava.nativelib.NativeLoader/loadLibrary "cvrptw_lib" nil)
         (org.scijava.nativelib.NativeLoader/loadLibrary "ortools" nil)
         (org.scijava.nativelib.NativeLoader/loadLibrary "jniortools" nil))
-      (org.scijava.nativelib.NativeLoader/loadLibrary "jniortools" nil))))
-(load-natives)
-
+      (org.scijava.nativelib.NativeLoader/loadLibrary "jniortools" nil)))
+  (reset! loaded true))
+(if (not @loaded) (load-natives))
 
 (defn new-solver [pname problem-type]
   (MPSolver.
    pname
    (MPSolver$OptimizationProblemType/valueOf problem-type)))
-
-(defn -main [& args]
-  (let [solver (new-solver "myprogram" "GLOP_LINEAR_PROGRAMMING")
-        x (.makeNumVar solver 0.0 1.0 "x")
-        y (.makeNumVar solver 0.0 2.0 "y")
-        objective (.objective solver)]
-    (.setMaximization objective)
-    (.solve solver)
-    (println "Solution:")
-    (println "x=" (.solutionValue x))
-    (println "y=" (.solutionValue x))))
