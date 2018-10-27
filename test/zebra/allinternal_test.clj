e93401432fe3e7299bda014e0e8c2ea1d341a00a
Nicolas Modrzyk <nico@karabiner-software.com>
2018-10-28 00:15:01 +0900

    comments, because I did not understand what was happening in the constraints

--- test/zebra/allinternal_test.clj
+++ test/zebra/allinternal_test.clj
@@ -11,14 +11,22 @@
         diffs (.makeIntVarArray solver (dec n) 1 (dec n) "diffs")
         db (.makePhase solver x Solver/CHOOSE_FIRST_UNBOUND Solver/ASSIGN_MIN_VALUE)]
 
+    ; all x should be different
     (.addConstraint solver (.makeAllDifferent solver x))
+    ; all diffs should be different
     (.addConstraint solver (.makeAllDifferent solver diffs))
 
     (dotimes [k (dec n)]
       (.addConstraint solver
+        ; difference between x and x+1 is equal to first diff
+        ; x    : 0 1 
+        ; diff : 1
+        ; because difference between 0 and 1 in the x values, is equal to absolute difference of 1
         (.makeEquality solver (nth diffs k) (.var (.makeAbs solver (.makeDifference solver (nth x (inc k)) (nth x k)))))))
 
+    ; the first x is lower than last x
     (.addConstraint solver (.makeLess solver (nth x 0) (nth x (dec n))))
+    ; first diff is lower than second diffs
     (.addConstraint solver (.makeLess solver (nth diffs 0) (nth diffs 1)))
 
     (.newSearch solver db)
@@ -34,4 +42,4 @@
 (deftest hello
   (solver-for-n 3)
   (solver-for-n 5)
-  )
+  (solver-for-n 12))
